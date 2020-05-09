33c8d3557d2e3601d9e14e51b8e99051edfdf87c
Rob Orsini <rob.orsini@autodesk.com>
2020-05-08 21:28:01 -0700

    moved code to lib/helpers

--- README.md
+++ README.md
@@ -1,6 +1,17 @@
-# pseudo-translate-json
+# Pseudo Translatify → «Pséüdö Třäñslätïfy»
+
 Pseudo-translates a directory of JSON while preserving variable interpolation
 
-## Build status
+## Instalation
+
+Options include:
+
+* Flexible variable interpolation syntax. `"MESSAGE_1": "Hello {{FIRST_NAME}} {{LAST_NAME}}, welcome in!"`
+
+* Include unicode "bookends" by defult to identify string concatentation issues.
+
+## Build status / ci + test coverage
 
 [![Node.js CI](https://github.com/rorsini/pseudo-translate-json/workflows/Node.js%20CI/badge.svg)](https://github.com/rorsini/pseudo-translate-json/actions?query=workflow%3A%22Node.js+CI%22)
+
+
--- index.js
+++ index.js
@@ -1,61 +1,13 @@
 const jsonfile = require('jsonfile');
-
-const CHARS = {
-    'a':'ä', 'c':'ç', 'i':'ï', 'C':'Ç',
-    'A':'Ä', 'e':'é', 'E':'É', 'D':'Ð',
-    'o':'ö', 'O':'Ö', 'u':'ü', 'U':'Ü',
-    'n':'ñ', 'r':'ř', 'Y':'Ý', 'w':'ω',
-    'N':'Ñ'
-}
-
-const _replaceChars = (str) => {
-    for (key in CHARS) {
-        str = str.replace(
-            RegExp(key, "g"),
-            CHARS[key]
-        );
-    }
-    return str;
-};
-
-const _processValue = (str) => {
-    const words = str.replace(/({{)\s+(\S+)\s+(}})/g, "$1﹏$2﹏$3").split(/ +/g);
-    let newWords = [];
-    for (word in words) {
-        const thisWord = words[word];
-        const re = new RegExp(/{{.*}}/);
-        if ( re.test(thisWord) ) {
-            const repairSpace = thisWord.replace(/﹏/g, ' ');
-            newWords.push(repairSpace);
-        } else {
-            newWords.push(_replaceChars(thisWord));
-        }
-    }
-    return newWords.join(' ');
-};
-
-const _wrapSentence = (str) => {
-    return `«${str}»`;
-};
-
-const pseudoTranslate = (obj) => {
-    if ( typeof obj === "object" ) {
-        let retObj = {};
-        for (key in obj) {
-            retObj[key] = pseudoTranslate(obj[key]);
-        }
-        return retObj;
-    } else if ( typeof obj === "string" ) {
-        const val = _wrapSentence(_processValue(obj));
-        return val;
-    }
-};
-
+const helpers = require('./lib/helpers');
 
 const fileName = "./test/fixtures/en-us/main.json";
 const enu_json = jsonfile.readFileSync(fileName);
-const pt_obj = pseudoTranslate(enu_json);
+const pt_obj = helpers.pseudoTranslate(enu_json);
+
+console.log("name: ", helpers.name);
+
 
 // jsonfile.writeFileSync(fileName, content, { spaces: 2 })
 
-exports.pseudoTranslate = pseudoTranslate;
+exports.pseudoTranslate = helpers.pseudoTranslate;
