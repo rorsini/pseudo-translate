5f0d7b740f0c1268668b8a6ced5f830c7e4e986c
Rob Orsini <rob.orsini@autodesk.com>
2020-05-09 07:50:56 -0700

    more cleanup

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
 const jsonfile = require('jsonfile');
 const ptify = require('./lib/ptify');
 
-const fileName = "./test/fixtures/en-us/main.json";
-const enu_json = jsonfile.readFileSync(fileName);
-const pt_obj = ptify.ptobj(enu_json);
-
 exports.ptify = ptify;
