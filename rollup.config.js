4f25cca5ac724f87f3a0be4b0a32ea7876d63a79
Rob Orsini <rob.orsini@autodesk.com>
2020-05-09 13:06:48 -0700

    another fs fix attempt

--- rollup.config.js
+++ rollup.config.js
@@ -3,19 +3,19 @@ import commonjs from '@rollup/plugin-commonjs';
 import pkg from './package.json';
 
 export default [
-	// browser-friendly UMD build
-	{
+    // browser-friendly UMD build
+    {
         input: 'src/ptify.js',
         external: ['fs'],
-		output: {
-			name: 'pseudoTranslatify',
-			file: pkg.browser,
+        output: {
+            name: 'pseudoTranslatify',
+            file: pkg.browser,
             format: 'umd',
             globals: {
                 fs: 'fs'
             }
-		},
-		plugins: [
+        },
+        plugins: [
             commonjs({
                 include: 'node_modules/**'
             }),
@@ -24,20 +24,32 @@ export default [
                 extensions: ['.js'],
                 preferBuiltins: true
             })
-		]
-	},
-	// CommonJS (for Node) and ES module (for bundlers) build.
-	// (We could have three entries in the configuration array
-	// instead of two, but it's quicker to generate multiple
-	// builds from a single configuration where possible, using
-	// an array for the `output` option, where we can specify
+        ]
+    },
+    // CommonJS (for Node) and ES module (for bundlers) build.
+    // (We could have three entries in the configuration array
+    // instead of two, but it's quicker to generate multiple
+    // builds from a single configuration where possible, using
+    // an array for the `output` option, where we can specify
     // `file` and `format` for each target)
-	{
+    {
         input: 'src/ptify.js',
         external: ['fs'],
-		output: [
-			{ file: pkg.main, format: 'cjs' },
-			{ file: pkg.module, format: 'es' }
-		]
-	}
+        output: [
+            {
+                file: pkg.main,
+                format: 'cjs',
+                globals: {
+                    fs: 'fs'
+                }
+            },
+            {
+                file: pkg.module,
+                format: 'es',
+                globals: {
+                    fs: 'fs'
+                }
+            }
+        ]
+    }
 ];
